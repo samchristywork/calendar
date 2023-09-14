7e171cddc399772941b68613651cc4eae500eac1
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-09-14 15:50:02 -0500

    feat: Add delay functions
    These functions allow me to add a delay to events. I can now, for
    instance, push back an event 1 hour using a single function call.

--- lib/cal.cpp
+++ lib/cal.cpp
@@ -56,6 +56,22 @@ Date::Date(string s) {
   day = stoi(tokens[2]);
 }
 
+void Date::delay(int year, int month, int day) {
+  this->year += year;
+  this->month += month;
+  this->day += day;
+
+  if (this->day > 31) {
+    this->month += this->day / 31;
+    this->day = this->day % 31;
+  }
+
+  if (this->month > 12) {
+    this->year += this->month / 12;
+    this->month = this->month % 12;
+  }
+}
+
 string Date::toString() {
   return to_string(year) + "-" + formatFixedWidth(to_string(month), 2, '0') +
          "-" + formatFixedWidth(to_string(day), 2, '0');
@@ -86,6 +102,26 @@ Time::Time(string s) {
   hour = stoi(tokens[0]);
   minute = stoi(tokens[1]);
   second = stoi(tokens[2]);
+}
+
+void Time::delay(Duration d) {
+  second += d.getSecond();
+  minute += d.getMinute();
+  hour += d.getHour();
+
+  if (second >= 60) {
+    minute += second / 60;
+    second = second % 60;
+  }
+
+  if (minute >= 60) {
+    hour += minute / 60;
+    minute = minute % 60;
+  }
+
+  if (hour >= 24) {
+    hour = hour % 24;
+  }
 }
 
 string Time::toString() {
@@ -121,11 +157,19 @@ DateTime::DateTime(string s) {
   time = new Time(tokens[1]);
 }
 
+void DateTime::delay(Duration d) {
+  time->delay(d);
+  if (time->getHour() == 0 && time->getMinute() == 0 &&
+      time->getSecond() == 0) {
+    date->delay(0, 0, 1);
+  }
+}
+
 string DateTime::toString() {
   return date->toString() + " " + time->toString();
 }
 
-class Duration {
+class Event {
 public:
   Event(string s);
   Event(string name, DateTime *dt, Duration *d) {
