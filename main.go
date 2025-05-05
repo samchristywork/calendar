fc293b5ccfaff5366a05dc7b1ed5631b146c522c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-05-05 14:00:14 -0500

    feat: Implement events handler for events.json endpoint
    This endpoint returns the contents of the `events.json` file. This file stores
    the user's calendar events.

--- main.go
+++ main.go
@@ -7,8 +7,31 @@ import (
 	"os"
 )
 
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		file, err := os.Open("data/events.json")
+		if err != nil {
+			http.Error(w, "Could not open file", http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+		data, err := ioutil.ReadAll(file)
+		if err != nil {
+			http.Error(w, "Could not read file", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	} else {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.Handle("/events.json", http.HandlerFunc(eventsHandler))
 	fmt.Println("Serving on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
